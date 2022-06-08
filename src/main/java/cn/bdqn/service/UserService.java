3b61b763729b4616226ffe9ae8ec64f79309ea94
HUAWEI <yx_0818@163.com>
2022-06-08 16:05:17 +0800

    my first springbootProject

---
+++ src/main/java/cn/bdqn/service/UserService.java
@@ -0,0 +1,17 @@
+package cn.bdqn.service;
+
+import cn.bdqn.pojo.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> list();
+
+    void add(User user);
+
+    void update(User user);
+
+    void delete(int id);
+
+    User getUserById(int id);
+}
